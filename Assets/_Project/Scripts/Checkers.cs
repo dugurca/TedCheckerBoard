4115f0eee5edf74928b03784a6d43da524b94061
Deniz Ugurca <dugurca@gmail.com>
2016-11-11 23:28:02 +0300

    .

---
+++ Assets/_Project/Scripts/Checkers.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+namespace Assets._Project.Scripts
+{
+    public class Checkers : MonoBehaviour
+    {
+        public GameObject BlackCube;
+        public GameObject WhiteCube;
+
+        void CreateCheckersBoard()
+        {
+            for (int row = 0; row < 8; row++)
+            {
+                for (int col = 0; col < 8; col++)
+                {
+                    GameObject cube;
+                    if ((row + col) % 2 == 0) cube = Instantiate(BlackCube);
+                    else cube = Instantiate(WhiteCube);
+                    cube.transform.position = new Vector3(col, row, 0);
+                }
+            }
+        }
+
+        // Use this for initialization
+        void Start () {
+            CreateCheckersBoard();
+        }
+	
+        // Update is called once per frame
+        void Update () {
+	
+        }
+    }
+}
